84e0d7abddd40fec2b259353d54544c027194711
marie7900 <68441734+marie7900@users.noreply.github.com>
2020-10-31 23:52:48 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-const socket = io();
+const socket = io(/hello);
 const params = new URLSearchParams(window.location.search);
 const username = params.get('username');
 const room = params.get('room');
